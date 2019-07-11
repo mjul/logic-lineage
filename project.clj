9a7462d0fae866f5779fb91061a245aa9d3dca99
Martin Jul <no-spam@mjul.com>
2019-07-11 21:50:24 +0200

    Declare use of MIT License in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject logic-lineage "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "Modelling Data Lineages With Logic Programming"
+  :url "http://mjul.com"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
 		 [org.clojure/core.logic "0.8.11"]]
   :repl-options {:init-ns logic-lineage.core}
